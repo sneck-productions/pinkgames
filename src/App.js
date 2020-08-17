166ab9fceea4feb92261191eb1b62e94dbf849ef
Malin <sneckproductions@gmail.com>
2020-08-17 16:11:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/*import React from "react";
+
+
+class App extends React.component {
+render() {
+ return <div>Hello world!</div>;
+}
+}
+
+//export default App;*/
+
+import React from "react";
+
+function App() {
+  return <div>Hello, world!</div>;
+}
+
+export default App;
